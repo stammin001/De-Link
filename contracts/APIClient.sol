a56b7a8aaf85e297fad33e3af49141f6b43f6450
stammin001 <stammin@gmail.com>
2022-05-22 19:22:12 -0500

    updates to API Client

--- contracts/APIClient.sol
+++ contracts/APIClient.sol
@@ -1,16 +1,22 @@
 // SPDX-License-Identifier: MIT
-// Deployed Rinkeby @ 0xEc2833eDDe62f700CC88a933097D6094883238a8
+// v1: Rinkeby @ 0xEc2833eDDe62f700CC88a933097D6094883238a8
+// v2: Rinkeby @ 0x9A63B12F6cc92889a52BeDBbD6E74Cfaf3CfF4cf
 pragma solidity ^0.8.7;
 
 import "./APIConsumer.sol";
 
 contract APIClient {
 
-    mapping(address => mapping(string => uint)) public creds;
-    string public constant URL = "https://my-json-server.typicode.com/stammin001/mock-api/users?";
+    string private constant URL = "https://my-json-server.typicode.com/stammin001/mock-api/users?";
     string private constant PATH = "0,graduated";
+    mapping(address => string[]) private docs;
 
     APIConsumer public consumer;
+
+    modifier isNotEmpty(string memory _input) {
+        require(bytes(_input).length !=0, "input string can't be empty");
+        _;
+    }
 
     constructor(address _consumer) {
         consumer = APIConsumer(_consumer);
@@ -20,12 +26,30 @@ contract APIClient {
         consumer = APIConsumer(_consumer);
     }
 
-    function validateCreds(string memory _input) public returns(bytes32) {
+    function validateCreds(string memory _input) public isNotEmpty(_input) returns(bytes32) {
         return consumer.requestValue(string(abi.encodePacked(URL, _input)), PATH);
     }
 
-    function getCreds(string memory _input) public view returns(uint256) {
+    function getCreds(string memory _input) public view isNotEmpty(_input) returns(uint256) {
         return consumer.getValue(string(abi.encodePacked(URL, _input)));
+    }
+
+    function uploadDoc(string memory _input) public isNotEmpty(_input) {
+        docs[msg.sender].push(_input);
+    }
+
+    function getDocs(address _input) public view returns(string[] memory) {
+        if(_input == address(0)) {
+            return docs[msg.sender];
+        }
+        return docs[_input];
+    }
+
+    function resetDocs(address _input) public {
+        if(_input == address(0)) {
+            docs[msg.sender] = new string[](0);
+        }
+        docs[_input] = new string[](0);
     }
 
 }
